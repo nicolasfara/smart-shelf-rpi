d6049961c07a9281fcd77d53d074e9035cb81796
Nicolas Farabegoli <nicolas.farabegoli@gmai.com>
2022-01-27 12:11:10 +0100

    feat: implemented logic for insert/remove products into/from the shelf

--- devices/product_manager.py
+++ devices/product_manager.py
@@ -5,12 +5,14 @@ import asyncio
 import boto3
 import os
 import logging
+from boto3.dynamodb.conditions import Attr
+from botocore.exceptions import ClientError
 from pathlib import Path
 from typing import List
 
 import aiofile
 import aiopubsub
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
 from marshmallow.exceptions import ValidationError
 from models.product import Product, ProductSchema
 
@@ -23,6 +25,10 @@ class ProductShelf:
 class ProductShelfSchema(Schema):
     """TODO"""
     products = fields.List(fields.Nested(ProductSchema))
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return ProductShelf(**data)
 
 
 class ProductManager:
@@ -57,17 +63,56 @@ class ProductManager:
 
     async def __on_new_tag(self, key, product: Product):
         self.__logger.info("Get new product %s from %s", product, key)
-        await self.__insert_remove_logic(product)
+        await self.__insert_remove_product_logic(product)
         self.__logger.debug("Product list: %s", self.__products.products)
 
-    async def __insert_remove_logic(self, product: Product) -> None:
-        pass
+    async def __insert_remove_product_logic(self, product: Product) -> None:
+        def callback():
+            try:
+                products_result = self.__table.scan(
+                    FilterExpression=Attr("code").eq("ABC12345") & Attr("lot").eq(250122)
+                )
+            except ClientError as error:
+                self.__logger.error(error)
+            else:
+                return products_result.get("Items", [])
+
+        result = await self.__loop.run_in_executor(None, callback)
+
+        if not result:
+            self.__logger.warning("No product with code %s and lot %s was found", product.code, product.lot)
+        else:
+            # The product exist in the DB
+            readed_product = ProductSchema().load(result[0])
+            self.__logger.debug("Now products %s", self.__products.products)
+            product_in_list = list(
+                filter(
+                    lambda p: p.code == readed_product.code and p.lot == readed_product.lot,
+                    self.__products.products
+                )
+            )
+
+            if not product_in_list:
+                self.__logger.debug("The product not exist, insert in the shelf")
+                self.__products.products.append(readed_product)
+            else:
+                self.__logger.debug("The product is in the shelf, remove it from shelf")
+                index = self.__products.products.index(product_in_list[0])
+                self.__products.products.pop(index)
+
+            await self.__write_products_file(ProductShelfSchema().dumps(self.__products))
 
     async def __load_products_file(self) -> None:
         async with aiofile.async_open(self.__save_path, 'r') as file:
             content = await file.read()
             try:
-                self.__products = ProductShelfSchema().load(content)
-            except ValidationError:
+                self.__products = ProductShelfSchema().loads(content)
+                self.__logger.debug("Load from file: %s", self.__products)
+            except ValidationError as error:
+                self.__logger.error(error)
                 self.__logger.debug("Failed to load products")
                 self.__logger.debug("Fall back to %s", self.__products.products)
+
+    async def __write_products_file(self, payload) -> None:
+        async with aiofile.async_open(self.__save_path, 'w') as file:
+            await file.write(payload)
